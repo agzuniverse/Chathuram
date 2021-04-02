fdda5a1a36565498212b97eff54336732088061f
agzuniverse <aswinganesh666@gmail.com>
2021-04-02 21:14:07 +0530

    Configure and add ESlint to github actions

---
+++ src/client/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    "plugins": [
+        "react"
+    ],
+    "parser": "babel-eslint",
+    // Add your custom rules here
+    rules: {
+    }
+}
