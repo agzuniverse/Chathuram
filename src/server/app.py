38ad3d6253b4cf795df5413a3bfae1c4faa5d1ae
agzuniverse <aswinganesh666@gmail.com>
2021-04-02 13:58:47 +0530

    Fix lint errors

--- src/server/app.py
+++ src/server/app.py
@@ -1,28 +1,32 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/data', methods=['GET'])
+@app.route("/data", methods=["GET"])
 def get_bulk_data():
     return jsonify({"data": "BULK DATA"})
 
-@app.route('/data/<id>', methods=['GET'])
+
+@app.route("/data/<id>", methods=["GET"])
 def get_single_data(id):
     return jsonify({"data": id})
 
-@app.route('/data', methods=['POST'])
+
+@app.route("/data", methods=["POST"])
 def post_data():
     return jsonify({"data": "RECORD CREATED"})
 
-@app.route('/data/<id>', methods=['PUT'])
+
+@app.route("/data/<id>", methods=["PUT"])
 def update_data(id):
     return jsonify({"data": "DATA UPDATED"})
 
-@app.route('/data/<id>', methods=['DELETE'])
+
+@app.route("/data/<id>", methods=["DELETE"])
 def delete_data(id):
     return jsonify({"data": "DATA DELETED"})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
