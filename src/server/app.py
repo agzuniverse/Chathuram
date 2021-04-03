a65d160d3073076b6644928de59e763ad45165e9
Arun <arunhari3210@gmail.com>
2021-04-03 22:55:21 +0530

    Login functonality changed to use environment variables

--- src/server/app.py
+++ src/server/app.py
@@ -1,55 +1,49 @@
-from flask import Flask, make_response
-from flask_mysqldb import MySQL
+from flask import Flask, request, make_response
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 import jwt
 import datetime
+import os
+import random
 
+# Init app
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()
 
-app.config["SECRET_KEY"] = "projectudap"
-
-# Config MySQL
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "arun"
-app.config["MYSQL_PASSWORD"] = "pass"
-app.config["MYSQL_DB"] = "myflaskapp"
-app.config["MYSQL_CURSORCLASS"] = "DictCursor"
-
-# init MYSQL
-mysql = MySQL(app)
+# Init database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + os.path.join(basedir, 'db.mysql')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route("/login", methods=["POST"])
-def login(data):
-    username = data.username
+@app.route("/login")
+def login():
+    data = request.authorization
+    username_candidate = data.username
     password_candidate = data.password
 
-    cur = mysql.connection.cursor()
-    res = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+    # Decrypt password here
 
-    if res > 0:
-        data = cur.fetchone()
-        password = data["password"]
+    username = os.getenv("LOGIN_USERNAME")
+    password = os.getenv("LOGIN_PASSWORD")
 
-        # Decrypt password
-
-        if password_candidate == password:
-            token = jwt.encode(
-                {
-                    "id": data["id"],
-                    "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
-                },
-                app.config["SECRET_KEY"],
-            )
-            return {"token": token.decode("UTF-8")}
-        else:
-            return make_response(
-                "Could not verify",
-                401,
-                {"WWW-authenticate": 'basic realm="Login Required"'},
-            )
-    return make_response(
-        "Could not verify", 401, {"WWW-authenticate": 'basic realm="Login Required"'}
-    )
+    if username_candidate == username and password_candidate == password:
+        token = jwt.encode(
+            {
+                "id": random.randint(0, 1000),
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+            },
+            app.config["SECRET_KEY"],
+        )
+        return {"token": token}
+    else:
+        return make_response(
+            "Could not verify",
+            401,
+            {"WWW-authenticate": 'basic realm="Login Required"'},
+        )
 
 
 @app.route("/data", methods=["GET"])
