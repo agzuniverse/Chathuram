28d64a8f739c52820ea3a2ec8477aed0bb8a8c1d
Megha K C <39724388+Megha-KC@users.noreply.github.com>
2021-05-15 23:45:47 +0530

    Update the /delete endpoint to handle the case when multiple rows are deleted

--- src/server/delete.py
+++ src/server/delete.py
@@ -11,19 +11,21 @@ from flask_cors import cross_origin
 def delete_table_data():
     data = request.get_json()
     table = data.get("table")
-    row = data.get("row")
+    rows = data.get("rows")
     if table not in db.get_tables_in_db():
         return {"error": "Table does not exist"}, 400
     current_table = Table(table, MetaData(), autoload_with=db.engine)
     column_names = current_table.c.keys()
-    row_to_be_deleted = {}
-    for idx in range(len(column_names)):
-        row_to_be_deleted[column_names[idx]] = row[idx]
-    try:
-        db.session.query(current_table).filter_by(**row_to_be_deleted).delete()
-        db.session.commit()
-    except Exception as e:
-        return {"message": e.__repr__()}, 400
+
+    for row in rows:
+        row_to_be_deleted = {}
+        for idx in range(len(column_names)):
+            row_to_be_deleted[column_names[idx]] = row[idx]
+        try:
+            db.session.query(current_table).filter_by(**row_to_be_deleted).delete()
+            db.session.commit()
+        except Exception as e:
+            return {"message": e.__repr__()}, 400
     return {"message": "Successfully Deleted"}, 200
 
 
