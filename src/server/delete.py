cc807d8f4d52b6bc7529df142399103c5c9e4da8
agzuniverse <aswinganesh666@gmail.com>
2021-06-06 20:02:42 +0530

    Restructure exceptions in delete functions

--- src/server/delete.py
+++ src/server/delete.py
@@ -25,9 +25,7 @@ def delete_table_data():
         try:
             db.session.query(current_table).filter_by(**row_to_be_deleted).delete()
             db.session.commit()
-        except OperationalError as e:
-            return {"error": "Failed to delete rows, {0}".format(e.orig)}, 400
-        except IntegrityError as e:
+        except (OperationalError, IntegrityError) as e:
             return {"error": "Failed to delete rows, {0}".format(e.orig)}, 400
     return {"message": "Successfully Deleted"}, 200
 
@@ -44,8 +42,6 @@ def delete_all_table_data():
     try:
         db.session.execute(current_table.delete())
         db.session.commit()
-    except Exception as e:
+    except (OperationalError, IntegrityError) as e:
         return {"error": "Failed to delete table, {0}".format(e.orig)}, 400
-    except IntegrityError as e:
-        return {"error": "Failed to delete rows, {0}".format(e.orig)}, 400
     return {"message": "Successfully Deleted All Rows"}, 200
