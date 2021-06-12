04af1fbf3cbaef52a50f8ac9fe8e6ce46b3258ed
Arun H <arunhari3210@gmail.com>
2021-06-13 01:11:48 +0530

    Read unit test working

--- src/client/src/tests/App.test.js
+++ src/client/src/tests/App.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import App from '../App';
 
-test('renders learn react link', () => {
+test('Running app', () => {
   const div = document.createElement("div")
     ReactDom.render(<App></App>, div)
 });
