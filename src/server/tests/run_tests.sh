0030ade7d16e2de01f4a3b8ff020da1c1835e59d
Megha K C <39724388+Megha-KC@users.noreply.github.com>
2021-06-14 12:17:37 +0530

    Change command for executing test.py due to change in directory structure

--- src/server/tests/run_tests.sh
+++ src/server/tests/run_tests.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-
 # POSTGRES
 eval `pifpaf run postgresql`
-
 # Run the tests
-cd ../..
-python3 -m server.tests.test
-
+cd ../
+python3 -m tests.test
 # Stop pifpaf
 pifpaf_stop
-
 # MYSQL
-
 # Run the tests
-
 # Stop pifpaf
-
-
