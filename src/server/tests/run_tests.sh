275217fa708e93b861ce5e8c4d67c38cb27241eb
Megha K C <39724388+Megha-KC@users.noreply.github.com>
2021-06-16 16:32:04 +0530

    Cleanup

--- src/server/tests/run_tests.sh
+++ src/server/tests/run_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # POSTGRES
-eval `pifpaf run postgresql`
+# eval `pifpaf run postgresql`
 # Run the tests
 cd ../
 python3 -m tests.test -v
 python3 -m tests.test_login -v
 python3 -m tests.test_config -v
 # Stop pifpaf
-pifpaf_stop
+# pifpaf_stop
 # MYSQL
 # Run the tests
 # Stop pifpaf
