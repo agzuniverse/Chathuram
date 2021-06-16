dc383ba0ab6aadbfddc3327813390c9cbfb74200
Megha K C <39724388+Megha-KC@users.noreply.github.com>
2021-06-16 12:50:08 +0530

    Perform integration test for delete and config handlers

--- src/server/tests/test.py
+++ src/server/tests/test.py
@@ -215,6 +215,47 @@ class TestDB(unittest.TestCase):
         )
         assert response.status_code == 200
 
+    # integration test for delete and config handlers
+    def test_delete(self):
+        client = app.test_client()
+        config_url = "/config"
+
+        token = jwt.encode(
+            {
+                "username": "user",
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=180),
+            },
+            app.config["SECRET_KEY"],
+            algorithm="HS256",
+        )
+
+        config_request_data = {
+            "username": "admin",
+            "password": "password",
+            "url": "localhost",
+            "port": 5432,
+            "db_name": "test",
+            "db_type": "postgres",
+        }
+
+        mock_request_headers = {
+            "Content-Type": "application/json",
+            "x-access-token": token,
+        }
+
+        client.post(
+            config_url,
+            data=json.dumps(config_request_data),
+            headers=mock_request_headers,
+        )
+
+        url = "/delete"
+        mock_request_data = {"table": "books", "rows": [["2", "Meluha", "Amish", True]]}
+        response = client.post(
+            url, data=json.dumps(mock_request_data), headers=mock_request_headers
+        )
+        assert response.status_code == 200
+
 
 if __name__ == "__main__":
     unittest.main()
