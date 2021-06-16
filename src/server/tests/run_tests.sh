f290f355901670c5363dcc36f9cab6228fd0862b
Megha K C <39724388+Megha-KC@users.noreply.github.com>
2021-06-16 11:42:06 +0530

    Run tests with more verbosity

--- src/server/tests/run_tests.sh
+++ src/server/tests/run_tests.sh
@@ -3,9 +3,9 @@
 eval `pifpaf run postgresql`
 # Run the tests
 cd ../
-python3 -m tests.test
-python3 -m tests.test_login
-python3 -m tests.test_config
+python3 -m tests.test -v
+python3 -m tests.test_login -v
+python3 -m tests.test_config -v
 # Stop pifpaf
 pifpaf_stop
 # MYSQL
